f01e8e95b5ec99f7bf5be3af8352dbe41acac4af
Eric Inácio <61947716+ericinacio@users.noreply.github.com>
2023-01-02 14:40:54 -0300

    Curso Dev Aprender

---
+++ Dev Aprender/Aula01/1-variaveis.js
@@ -0,0 +1,27 @@
+// camelCase - Maneira certa de nomear variáveis (Ex: nomeCompleto)
+
+let nao = "Não";
+let a = "á"
+let teste = "Teste"
+console.log(nao);
+console.log(a);
+console.log(teste);
+
+// [ let ] - Pode ser alterado ao longo do código.
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
+
+// [ const ] - Não pode ser alterado ao longo do código.
+const valorIngresso = 20;
+console.log(valorIngresso);
+
+// Tipos Primitivos de Definição de Variáveis
+let nome = "Rafael"; //String (Escritas)
+let idade = 25; //Number (Números)
+let estaAprovado = true; //Boolean (True ou False)
+let sobrenome = undefined; // Undefined (Indefinido - Não Usar)
+let corSelecionado = null // Redefinir um valor
+
+
+// typeof - Para verificar o tipo
---
+++ Dev Aprender/Aula01/3-objetos.js
@@ -0,0 +1,10 @@
+// Criando um Objeto
+let pessoa = {
+  // Key    Value
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de Souza'
+};
+
+console.log(pessoa);
